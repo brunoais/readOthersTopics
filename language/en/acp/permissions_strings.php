ca62b4527b8ff59bbd89bb099f3d61f7682f6f0f
brunoais <brunoaiss@gmail.com>
2015-01-25 23:15:19 +0000

    Initial permission strings

---
+++ language/en/acp/permissions_strings.php
@@ -0,0 +1,14 @@
+<?php
+/**
+*
+* @package phpBB Extension - brunoais readOthersTopics
+* @copyright (c) 2015 phpBB Group
+* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+*
+*/
+
+$lang = array_merge($lang, array(
+
+	'ACL_F_READ_OTHERS_TOPICS' => 'Can read topics started by others',
+
+));
