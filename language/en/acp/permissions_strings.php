de4b53601db791e696b238508cdd332a51415f89
brunoais <brunoaiss@gmail.com>
2015-02-05 10:59:11 +0000

    [language] Added what should be the last missing lang keys

--- language/en/acp/permissions_strings.php
+++ language/en/acp/permissions_strings.php
@@ -10,5 +10,7 @@
 $lang = array_merge($lang, array(
 
 	'ACL_F_READ_OTHERS_TOPICS' => 'Can read topics started by others',
+	'BRUNOAIS_ROLE_READ_OTHERS_TOPICS' => 'Standard Access  w/o topics made by others',
+	'BRUNOAIS_ROLE_DESCRIPTION_READ_OTHERS_TOPICS' => 'Can use most forum features just like standard access except viewing topics that were initiated by other users.',
 
 ));
