5c10991b0f3b93caeeecdbd2c494be457d2ea692
brunoais <brunoaiss@gmail.com>
2015-08-22 10:31:53 +0100

    [main_listener] search_mysql_author_query_before

--- event/main_listener.php
+++ event/main_listener.php
@@ -56,6 +56,8 @@ class main_listener implements EventSubscriberInterface
 			'core.search_mysql_keywords_main_query_before'			=> 'search_mysql_keywords_main_query_before',
 			'core.search_native_keywords_count_query_before'		=> 'search_native_keywords_count_query_before',
 			'core.search_postgres_keywords_main_query_before'		=> 'search_postgres_keywords_main_query_before',
+
+			'core.search_mysql_author_query_before'					=> 'search_mysql_author_query_before',
 		);
 	}
 
@@ -746,6 +748,80 @@ class main_listener implements EventSubscriberInterface
 			}
 		}
 	}
+
+	public function search_mysql_author_query_before($event){
+
+		$topic_id = $event['topic_id'];
+
+		if(empty($topic_id)){
+
+			$forums_permissions = $this->auth->acl_get_list($this->user->data['user_id'], array('f_read', 'f_read_others_topics_brunoais'));
+
+			$ex_fid_keys = array_keys($event['ex_fid_ary']);
+
+			$partial_read_access_fids = $full_read_access_fids = array();
+
+			foreach($forums_permissions as $forum_id => $forum_permissions){
+
+				if(isset($forum_permissions['f_read']) &&
+					!isset($ex_fid_keys[$forum_id])){
+					if(isset($forum_permissions['f_read_others_topics_brunoais'])){
+						$full_read_access_fids[$forum_id] = $forum_id;
+					}else{
+						$partial_read_access_fids[$forum_id] = $forum_id;
+					}
+				}
+			}
+
+			if(sizeof($partial_read_access_fids) > 0){
+				// The filter has to be in place
+
+				// Workaround for a mistake I made myself when making the event.
+				$sql_sort_join = $event['sql_sort_join'];
+				$sql_sort_join .= ' AND t_brunoais.topic_id = p.topic_id ';
+				$event['sql_sort_join'] = $sql_sort_join;
+
+				$sql_sort_table = $event['sql_sort_table'];
+				$sql_sort_table .= TOPICS_TABLE . ' t_brunoais, ';
+				$event['sql_sort_table'] = $sql_sort_table;
+
+
+				$sql_fora = $event['sql_fora'];
+
+				$sql_fora .= ' AND (' . $this->db->sql_in_set('t_brunoais.forum_id', $full_read_access_fids, false, true) . '
+					OR t_brunoais.topic_poster = ' . (int) $this->user->data['user_id'] . ' )';
+
+				$event['sql_fora'] = $sql_fora;
+
+			}
+
+
+		}else{
+			$permissionResult = $this->permissionEvaluate(array(
+				'topic_id' => $topic_id,
+			));
+
+			if($permissionResult === 'NO_READ_OTHER'){
+
+				// Workaround for a mistake I made myself when making the event.
+				$sql_sort_join = $event['sql_sort_join'];
+				$sql_sort_join .= ' AND t_brunoais.topic_id = p.topic_id ';
+				$event['sql_sort_join'] = $sql_sort_join;
+
+				$sql_sort_table = $event['sql_sort_table'];
+				$sql_sort_table .= TOPICS_TABLE . ' t_brunoais, ';
+				$event['sql_sort_table'] = $sql_sort_table;
+
+
+				$sql_fora = $event['sql_fora'];
+
+				$sql_fora .= ' AND t.topic_poster = ' . (int) $this->user->data['user_id'];
+
+				$event['sql_fora'] = $sql_fora;
+			}
+		}
+	}
+
 	//
 	// Auxiliary functions
 	//
