8b6b7d72e5df6b5b41be0b359f0a266b92ed9c26
Heinrich Krebs <hchkrebs@web.de>
2018-08-06 17:11:36 +0200

    removed double and tailing space

--- language/de_x_sie/acp/permissions_strings.php
+++ language/de_x_sie/acp/permissions_strings.php
@@ -22,6 +22,6 @@ if (empty($lang) || !is_array($lang))
 // All language files should use UTF-8 as their encoding and the files must not contain a BOM.
 $lang = array_merge($lang, array(
 	'ACL_F_READ_OTHERS_TOPICS' => 'Kann von anderen gestartete Themen lesen',
-	'BRUNOAIS_ROLE_READ_OTHERS_TOPICS' => 'Standard-Zugang  ohne von anderen gestartete Themen ',
+	'BRUNOAIS_ROLE_READ_OTHERS_TOPICS' => 'Standard-Zugang ohne von anderen gestartete Themen',
 	'BRUNOAIS_ROLE_DESCRIPTION_READ_OTHERS_TOPICS' => 'Kann die meisten Forenfunktionen nutzen, wie Standard-Zugang, darf aber keine von anderen Nutzern gestartete Themen lesen.',
 ));
